1df8e130acdb2492d7cec8ad87c4e67c3cfcc51b
Felipe De Andrade <felipe.deam98@gmail.com>
2026-05-24 13:34:39 -0300

    refactor(components): rename Navegacao to Navigation

---
+++ src/components/Menu/Navigation/index.tsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+import classNames from "classnames";
+import CrownSimple from "../../../assets/svg/CrownSimple.svg?react";
+import style from "./styles.module.scss";
+
+interface INavigation {
+  tamanhoMenu: number;
+  menuRef: React.RefObject<HTMLDivElement>;
+}
+
+const Navigation: React.FC<INavigation> = ({ tamanhoMenu, menuRef }) => {
+  return (
+    <div
+      className={style.navigation}
+      style={{ transform: `translateX(${tamanhoMenu}px)` }}
+      ref={menuRef}
+    >
+      <Link className={style.navigation__link} to="#">
+        Todas Categorias
+      </Link>
+      <Link className={style.navigation__link} to="#">
+        Supermercado
+      </Link>
+      <Link className={style.navigation__link} to="#">
+        Livros
+      </Link>
+      <Link className={style.navigation__link} to="#">
+        Moda
+      </Link>
+      <Link className={style.navigation__link} to="#">
+        Lançamentos
+      </Link>
+      <Link
+        className={classNames(
+          [style.navigation__link],
+          [style["navigation__link--highlight"]],
+        )}
+        to="#"
+      >
+        Ofertas do dia
+      </Link>
+      <Link className={style.navigation__link} to="#">
+        <CrownSimple
+          className={classNames([style["navigation__link--crown"]])}
+        />
+        Assinatura
+      </Link>
+    </div>
+  );
+};
+
+export default Navigation;
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
-import style from './Menu.module.scss';
+import React, { useEffect, useRef, useState } from "react";
+import style from "./Menu.module.scss";
 
-import Top from './Top';
-import Mid from './Mid';
-import Navegacao from './Navegacao';
+import Top from "./Top";
+import Mid from "./Mid";
+import Navigation from "./Navigation";
 
 const Menu: React.FC = () => {
   const [menu, setMenu] = useState<boolean>(false);
@@ -11,25 +11,25 @@ const Menu: React.FC = () => {
   const menuRef = useRef<HTMLDivElement>(null);
 
   const calculaMenu = () => {
-    const valorMenu = Number(menuRef.current?.clientWidth)
+    const valorMenu = Number(menuRef.current?.clientWidth);
     setTamanhoMenu(menu ? 0 : valorMenu * 1);
-  }
+  };
 
   useEffect(() => {
-    if(window.innerWidth < 768) {
-      calculaMenu()
+    if (window.innerWidth < 768) {
+      calculaMenu();
     }
-  })
+  });
 
   return (
     <header>
       <nav className={style.menu}>
         <Top />
         <Mid setMenu={setMenu} menuOpen={menu} calculaMenu={calculaMenu} />
-        <Navegacao tamanhoMenu={tamanhoMenu} menuRef={menuRef} />
+        <Navigation tamanhoMenu={tamanhoMenu} menuRef={menuRef} />
       </nav>
     </header>
-  )
-}
+  );
+};
 
 export default Menu;

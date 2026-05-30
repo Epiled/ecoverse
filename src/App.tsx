bdc85daac85049b468be6aa2d2699f43f027210a
Felipe De Andrade <felipe.deam98@gmail.com>
2026-05-30 04:24:34 -0300

    refactor(styles): rename files to english and update imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import './styles/EstilosGlobais.module.scss'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+import "./styles/Globals.module.scss";
+
 const App: React.FC = () => {
-
   return (
-      <BrowserRouter>
+    <BrowserRouter>
+      {/* Routes */}
+      <Routes>
+        <Route index path="/" element={<Home />} />
 
-        {/* Rotas */}
-        <Routes>
-          <Route index path='/' element={<Home />} />
+        {/* Fallback route */}
+        <Route path="/404" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-          {/* Rota de fallback */}
-          {/* <Route path='/404' element={<Home />} /> */}
-          {/* <Route path='*' element={<Home />} /> */}
-        </Routes>
-
-      </BrowserRouter>
-  )
-}
-
-export default App
+export default App;

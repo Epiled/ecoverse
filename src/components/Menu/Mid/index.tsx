9a05ef3509dafe97cc6ffe887eef5a80e4393020
Felipe De Andrade <felipe.deam98@gmail.com>
2026-05-30 05:56:46 -0300

    chore(icons): organize UI icons structure

--- src/components/Menu/Mid/index.tsx
+++ src/components/Menu/Mid/index.tsx
@@ -2,13 +2,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import Group from "@/assets/svg/group.svg?react";
-import Heart from "@/assets/svg/heart.svg?react";
-import User from "@/assets/svg/user.svg?react";
-import ShoppingCart from "@/assets/svg/shopping-cart.svg?react";
-import MagnifyingGlass from "@/assets/svg/magnifying-glass.svg?react";
-import Menu from "@/assets/svg/menu.svg?react";
-import Close from "@/assets/svg/close.svg?react";
+import Group from "@/assets/svg/icons-ui/group.svg?react";
+import Heart from "@/assets/svg/icons-ui/heart.svg?react";
+import User from "@/assets/svg/icons-ui/user.svg?react";
+import ShoppingCart from "@/assets/svg/icons-ui/shopping-cart.svg?react";
+import MagnifyingGlass from "@/assets/svg/icons-ui/magnifying-glass.svg?react";
+import Menu from "@/assets/svg/icons-ui/menu.svg?react";
+import Close from "@/assets/svg/icons-ui/close.svg?react";
 import logo from "@/assets/img/webp/logo.webp";
 
 import style from "./styles.module.scss";

55d2f1c0d4fe43cb363a513e2c90aefb99198cca
Felipe De Andrade <felipe.deam98@gmail.com>
2026-06-01 06:37:18 -0300

    chore(deps): clean up unused packages and upgrade existing dependencies

--- src/service/useProducts.ts
+++ src/service/useProducts.ts
@@ -2,11 +2,6 @@ import { IProduct } from "../interfaces/IProduct";
 
 import useFetch from "./useFetch";
 
-// const apiURLs = {
-//   development: "http://localhost:3000/products", // JSON Server
-//   production: "https://api-ecoverse.vercel.app/api/produtos" // API de produção
-// };
-
 const useProducts = () => {
   return useFetch<IProduct[]>({ url: 'products' }); // Use a URL do endpoint
 
